e14c28b392794c3c5e0a413513a2ae0bd449018a
Tom White <tom.e.white@gmail.com>
2025-12-29 10:34:54 +0000

    Add notes

--- README.md
+++ README.md
@@ -4,14 +4,3 @@ Operations
 * Append to a Zarr store (atomically)
 * Delete values from a Zarr store (atomically)
 * Tag a Zarr store
-
-This shows how we can remove a sample from a Zarr store (zarr-python/format v2 currently)
-
-```shell
-conda activate vczlib-poc-zarr-v2
-rm -rf sample.vcf.vcz; cp -r ~/workspace/vcztools/vcz_test_cache/sample.vcf.vcz sample.vcf.vcz
-pytest -vs tests/test_remove.py
-
-# then in vcztools dir, delete-mask branch, vcztools-3.12 venv
-python -m vcztools view ~/workspace/vczlib-poc/sample.vcf.vcz
-```
--- tests/test_remove.py
+++ tests/test_remove.py
@@ -2,8 +2,11 @@
 # For the moment don't worry about atomicity or transactions, as we can use icechunk later for that.
 # But we do need to consider masking and recomputing fields like AC.
 
+# conda activate vczlib-poc-zarr-v2
 # rm -rf sample.vcf.vcz; cp -r ~/workspace/vcztools/vcz_test_cache/sample.vcf.vcz sample.vcf.vcz
+# pytest -vs tests/test_remove.py
 
+# then in vcztools dir, delete-mask branch, vcztools-3.12 venv
 # python -m vcztools query -f '[%SAMPLE %GT %DP\n]' -s NA00001,NA00003 ~/workspace/vczlib-poc/sample.vcf.vcz
 # python -m vcztools view ~/workspace/vczlib-poc/sample.vcf.vcz
 # python -m vcztools view -s NA00001,NA00003 ~/workspace/vczlib-poc/sample.vcf.vcz
